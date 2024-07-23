35eb4630323392db22be0da48ffbe7e37273a564
jonywu <jonywu@mitac.com.tw>
2024-07-23 20:14:17 +0800

    安裝套件:  vite svg loader

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
                 defineModel: true,
             },
         }),
+        svgLoader(),
     ],
 });

0abc422b0b36d6a95aecd09c486e90cf1c2cfa52
jonyworker <jonywu@gmail.com>
2024-07-24 02:16:30 +0800

    feat: Divider 完成

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,43 @@ import HelloWorld from "./components/HelloWorld.vue";
 import ACol from "./components/layout/Col.vue";
 import ARow from "./components/layout/Row.vue";
 import AContainer from "./components/layout/Container.vue";
-import AIcon from "./components/atom/BaseIcon.vue";
+import AIcon from "@/components/atom/BaseIcon.vue";
+import ADivider from "@/components/layout/Divider.vue";
 </script>
 
 <template>
     <a-icon name="academi" size="20"></a-icon>
-
+    <a-container size="sm" class="border-1">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            iure vero, minus laborum laboriosam expedita perferendis dolores
+            aliquid aspernatur in blanditiis maiores. Beatae itaque voluptatum
+            dignissimos, molestiae quidem ipsa veritatis?
+        </p>
+    </a-container>
+    <div class="d-flex flex-column" style="border: 1px solid red">
+        <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <a-divider></a-divider>
+        <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
     <div class="icon">
-        <a-icon name="finger-print"></a-icon>
+        <a-icon name="finger-print" size="50" vertical></a-icon>
     </div>
 </template>
 
---
+++ src/components/layout/Divider.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, computed } from "vue";
+const props = defineProps({
+    thickness: {
+        type: [Number, String],
+        default: "1",
+    },
+    color: {
+        type: String,
+        default: "#334155",
+        TODO: 顏色確認後修改,
+        // validator: (value) =>
+        //     ["primary", "secondary", "success", "danger", "warning"].includes(
+        //         value
+        //     ),
+    },
+    vertical: {
+        type: Boolean,
+    },
+});
+console.log(typeof props.thickness);
+const borderThickness = computed(() => {
+    return {
+        height: props.vertical ? "100%" : `${props.thickness}px`,
+        width: props.vertical ? `${props.thickness}px` : "100%",
+        margin: props.vertical ? "0 1rem" : "1rem 0",
+        backgroundColor: `${props.color}`,
+    };
+});
+</script>
+
+<template>
+    <div class="divider" :style="borderThickness"></div>
+</template>
+
+<style scoped>
+.divider {
+    flex-shrink: 0;
+    opacity: 0.15;
+}
+</style>

87d569937537fdd98674b48583e3e4b419f32dec
jonyworker <jonywu@gmail.com>
2024-07-25 01:21:36 +0800

    update: 修改 main.js CSS 讀取連結

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
-import "./assets/css/bootstrap.css";
+// import "./assets/css/bootstrap.css";
+import "./assets/css/globals.css";
+import "./assets/css/custom.css";
 import App from "./App.vue";
 
 createApp(App).mount("#app");

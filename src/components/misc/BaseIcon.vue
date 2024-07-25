0b41f617a82aec0e2e1d6b1c31e9afc67d03d4d4
jonyworker <jonywu@gmail.com>
2024-07-25 19:03:16 +0800

    move: 移動資料位置

---
+++ src/components/misc/BaseIcon.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { computed, ref, defineAsyncComponent } from "vue";
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    size: {
+        type: String,
+        required: false,
+        default: 24,
+        // validator: (value) =>
+        //     ["primary", "secondary", "success", "danger", "warning"].includes(
+        //         value
+        //     ),
+    },
+    color: {
+        type: String,
+        default: "#000",
+    },
+});
+
+const iconComponent = defineAsyncComponent(() =>
+    import(`../../assets/icons/${props.name}.svg`)
+);
+</script>
+
+<template>
+    <component
+        :is="iconComponent"
+        :width="props.size"
+        :height="props.size"
+        :stroke="props.color"
+    />
+</template>
+
+<style lang="scss" scoped></style>

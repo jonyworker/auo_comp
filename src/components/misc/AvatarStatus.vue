c99ec2f08d6497c16e541ef094748854d9556817
jonyworker <jonywu@gmail.com>
2024-07-27 17:49:52 +0800

    feat: 新增 AvatarStatus 元件

---
+++ src/components/misc/AvatarStatus.vue
@@ -0,0 +1,142 @@
+<script setup>
+import { ref, computed } from "vue";
+import { cva } from "class-variance-authority";
+
+const props = defineProps({
+    status: {
+        type: String,
+        validator: (value) =>
+            ["online", "busy", "away", "offline"].includes(value),
+    },
+    rounded: Boolean,
+    position: {
+        type: String,
+        validator: (value) => ["tl", "tr", "br", "bl"].includes(value),
+    },
+});
+
+const avatarStatusCVAClass = computed(() => {
+    return cva("avatar-container avatar-status", {
+        variants: {
+            status: {
+                online: "status-online",
+                busy: "status-busy",
+                away: "status-away",
+                offline: "status-offline",
+            },
+        },
+        compoundVariants: [
+            // status position(tl, tr, br, bl), rounded: false
+            {
+                position: "tl",
+                rounded: false,
+                class: "status-position-tl",
+            },
+            {
+                position: "tr",
+                rounded: false,
+                class: "status-position-tr",
+            },
+            {
+                position: "br",
+                rounded: false,
+                class: "status-position-br",
+            },
+            {
+                position: "bl",
+                rounded: false,
+                class: "status-position-bl",
+            },
+
+            // status position(tl, tr, br, bl), rounded: true
+            {
+                position: "tl",
+                rounded: true,
+                class: "status-position-tl-rounded",
+            },
+            {
+                position: "tr",
+                rounded: true,
+                class: "status-position-tr-rounded",
+            },
+            {
+                position: "br",
+                rounded: true,
+                class: "status-position-br-rounded",
+            },
+            {
+                position: "bl",
+                rounded: true,
+                class: "status-position-bl-rounded",
+            },
+        ],
+    })({
+        //這裡設定 variants名稱接收 props的值
+        status: props.status,
+        rounded: props.rounded,
+        position: props.position,
+    });
+});
+</script>
+
+<template>
+    <span :class="avatarStatusCVAClass"></span>
+</template>
+
+<style scoped>
+.avatar-container .avatar-status {
+    position: absolute;
+    width: 12px;
+    height: 12px;
+    border-radius: 9999px;
+    border: 2px solid #1f2937;
+}
+.avatar-status.status-position-tl {
+    top: -5px;
+    left: -5px;
+}
+.avatar-status.status-position-tr {
+    top: -5px;
+    right: -5px;
+}
+.avatar-status.status-position-br {
+    bottom: -5px;
+    right: -5px;
+}
+.avatar-status.status-position-bl {
+    bottom: -5px;
+    left: -5px;
+}
+.avatar-status.status-position-tl-rounded {
+    top: 0;
+    left: 0;
+}
+.avatar-status.status-position-tr-rounded {
+    top: 0;
+    right: 0;
+}
+.avatar-status.status-position-br-rounded {
+    bottom: 0;
+    right: 0;
+}
+.avatar-status.status-position-bl-rounded {
+    bottom: 0;
+    left: 0;
+}
+.avatar-status {
+    /* green */
+    background-color: #31c48d;
+}
+.avatar-status.status-busy {
+    /* yellow */
+    background-color: #e3a008;
+}
+.avatar-status.status-away {
+    /* gray */
+    background-color: #9ca3af;
+}
+.avatar-status.status-offline {
+    /* red */
+    background-color: #f98080;
+}
+</style>

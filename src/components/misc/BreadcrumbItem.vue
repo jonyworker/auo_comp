942fa991ea358ad4e65367e542abd2becabef15f
jonyworker <jonywu@gmail.com>
2024-07-27 18:10:04 +0800

    style: 新增 svg icon

---
+++ src/components/misc/BreadcrumbItem.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+import AIcon from "@/components/misc/BaseIcon.vue";
+</script>
+
+<template>
+    <div>
+        <a-icon name="finger-print" size="20"></a-icon>
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="" scoped></style>
